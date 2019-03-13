d54e67ffe4e158f17042bf759b2a23f313e97b47
candeogi <giovanni.candeo.1@gmail.com>
2019-03-13 18:37:42 +0100

    clean code and complete (?) homework 1

--- src/main/java/G32HM1.java
+++ src/main/java/G32HM1.java
@@ -1,17 +1,19 @@
+import edu.stanford.nlp.coref.statistical.StatisticalCorefAlgorithm;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.execution.columnar.DOUBLE;
-import org.codehaus.janino.Java;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.stat.MultivariateStatisticalSummary;
+import org.apache.spark.mllib.stat.Statistics;
+import org.apache.spark.rdd.RDD;
 
-import javax.print.DocFlavor;
+import java.io.DataOutput;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 
 public class G32HM1 {
 
@@ -58,21 +60,37 @@ public class G32HM1 {
         //parallel collection
         JavaRDD<Double> dNumbers = sparkContext.parallelize(lNumbers);
 
+        //finds and prints the maximum number using reduce method
         double maxNumberReduce = dNumbers.reduce(G32HM1::findMax);
         System.out.println("Reduce method: the max number is "+maxNumberReduce);
 
-
-//        double maxNumberMax = dNumbers.max(new valueComparator());
-//        System.out.println("Max method: the max number is "+maxNumberMax);
+        //finds and prints the maximum number using max method
+        double maxNumberMax = dNumbers.max(new valueComparator());
+        System.out.println("Max method: the max number is "+maxNumberMax);
 
         //creates a RDD of normalized numbers
-        JavaRDD<Double> dNumbersNormalized = dNumbers.map(x -> x/maxNumberReduce);
+        JavaRDD<Double> dNormalized = dNumbers.map(x -> x/maxNumberReduce);
 
-        //prints the content of the RDD - is this a statistic?
-        List<Double> lNumbersNormalized = dNumbersNormalized.collect();
-        lNumbersNormalized.forEach((temp)->{
-            System.out.println(temp);
-        });
+        /*
+        The following code compute and print median value of the RDD dNormalized
+        */
+
+        //count how many elements contains
+        long count = dNormalized.count();
+        System.out.println("RDD contains "+count+" objects");
+
+        //sum the value of all elements
+        double sum = 0;
+        List<Double> lNormalized = dNormalized.collect();
+        for(Double listElement: lNormalized){
+                    //System.out.println(listElement);
+                    sum += listElement;
+        }
+
+        //compute the median value
+        double media = sum/count;
+        System.out.println("Il valore medio è: "+media);
 
     }
+
 }
