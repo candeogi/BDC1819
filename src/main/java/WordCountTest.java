8df2349bcd48bb250f47528eb72ccd631fce9f9c
candeogi <giovanni.candeo.1@gmail.com>
2019-04-01 17:21:08 +0200

    add time measurement and comments.

--- src/main/java/WordCountTest.java
+++ src/main/java/WordCountTest.java
@@ -4,13 +4,14 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
+import java.util.*;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
+/**
+ * This class is a remake of the SecondTemplate file provided from the homework 2.
+ * The class implement a word count algorithm as in the SecondTemplate but uses methods instead of lambda functions.
+ * Also makes a time measurement of the time needed for the algorithm to complete.
+ * @author Giovanni Candeo
+ */
 public class WordCountTest {
     public static void main(String[] args){
 
@@ -29,29 +30,26 @@ public class WordCountTest {
         // #words 3503570
 
         //RDD contains 10122 docs composed by a single line of numerous strings
-        JavaRDD<String> document = sc.textFile(args[0]);
-        System.out.println("TEST COUNT: "+document.count());
+        JavaRDD<String> document = sc.textFile(args[0]).cache();
+        System.out.println("Test count: "+document.count());
 
+        //lets start measuring time from here
+        long start = System.currentTimeMillis();
 
         //Iterator<Tuple2<String, Long>> countForSingleWord = document.flatMapToPair(WordCountTest::countSingleWords);
-        JavaPairRDD<String,Long> wordcountpairs = document
+        JavaPairRDD<String,Long> dWordCountPairs = document
                 .flatMapToPair(WordCountTest::countSingleWords)
                 .groupByKey()
                 .mapValues(WordCountTest::countOccurrences);
 
+        //end of time measuring
+        long end = System.currentTimeMillis();
+        System.out.println("Elapsed time: " + (end - start) + " ms");
 
     }
 
-    private static <U> Long countOccurrences(Iterable<Long> iterable) {
-        Long sum = Long.valueOf(0);
-        for(Long c: iterable){
-            sum += c;
-        }
-        return sum;
-    }
 
-
-    private static <K2,V2> Iterator<Tuple2<String,Long>> countSingleWords(String s) {
+    private static Iterator<Tuple2<String,Long>> countSingleWords(String s) {
         String[] words = s.split(" ");
         ArrayList<Tuple2<String, Long>> counts = new ArrayList<>();
         for(String w : words){
@@ -59,6 +57,14 @@ public class WordCountTest {
             counts.add(new Tuple2<>(w,1L));
         }
         return counts.iterator();
+    }
+
+    private static Long countOccurrences(Iterable<Long> iterable) {
+        Long sum = Long.valueOf(0);
+        for(Long c: iterable){
+            sum += c;
+        }
+        return sum;
     }
 
 
